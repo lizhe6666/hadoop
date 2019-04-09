8d79455af5a2d9ccd91eaf1876e0bf132c6fd32d
lizhe <lizhe@baice100.com>
2019-04-09 15:20:05 +0800

    2

--- src/main/java/utils/GetIp.java
+++ src/main/java/utils/GetIp.java
@@ -29,6 +29,7 @@ public class GetIp {
         else {
             System.err.println("ip地址查询失败，请检查ip地址是否正确");
             System.out.println("merge into master");
+            System.out.println("2");
         }
     }
 
